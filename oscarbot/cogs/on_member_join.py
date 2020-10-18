c54a5bfa8e08c7a61e9a12c19a5c53f2550617ec
bonelesspizz <38578849+bonelesspizz@users.noreply.github.com>
2020-10-18 20:49:07 +0100

    Fix cogs

---
+++ oscarbot/cogs/on_member_join.py
@@ -0,0 +1,14 @@
+import discord
+from discord.ext import commands
+
+class OnMemberJoin(commands.Cog):
+    def __init__(self, oscar):
+        self.oscar = oscar
+
+    @commands.Cog.listener()
+    async def on_member_join(self, member):
+        channel = member.guild.system_channel
+        await channel.send(f"Welcome {member.mention} to the server!")
+
+def setup(oscar):
+    oscar.add_cog(OnMemberJoin(oscar))
--- oscarbot/main.py
+++ oscarbot/main.py
@@ -9,8 +9,21 @@ token = os.getenv('DISCORD_TOKEN')
 
 oscar = commands.Bot(command_prefix='!')
 
+@oscar.event
+async def on_ready():
+    print("Ready")
+
 @oscar.command()
 async def load(ctx, extension):
     oscar.load_extension(f'cogs.{extension}')
+
+@oscar.command()
+async def unload(ctx, extension):
+    oscar.unload_extension(f'cogs.{extension}')
+
+for filename in os.listdir("./cogs"):
+    if filename.endswith(".py"):
+        oscar.load_extension(f"cogs.{filename[:-3]}")
+
 
 oscar.run(token)
