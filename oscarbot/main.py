d953197afb3e83e06b85fa72f0d48cc04fba9a17
bonelesspizz <38578849+bonelesspizz@users.noreply.github.com>
2020-10-18 20:09:40 +0100

    Fix cogs

--- oscarbot/main.py
+++ oscarbot/main.py
@@ -9,4 +9,8 @@ token = os.getenv('DISCORD_TOKEN')
 
 oscar = commands.Bot(command_prefix='!')
 
+@oscar.command()
+async def load(ctx, extension):
+    oscar.load_extension(f'cogs.{extension}')
+
 oscar.run(token)
