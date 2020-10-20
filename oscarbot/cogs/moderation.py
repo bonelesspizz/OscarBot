6118507a0d12733a5c83997d43a246a4ac440323
bonelesspizz <38578849+bonelesspizz@users.noreply.github.com>
2020-10-20 21:23:59 +0100

    Fix unban command

--- oscarbot/cogs/moderation.py
+++ oscarbot/cogs/moderation.py
@@ -11,9 +11,15 @@ class Moderation(commands.Cog):
         await ctx.send(f"{member.mention} has been banned.")
 
     @commands.command()
-    async def unban(self, ctx, user:discord.User=None):
-        await guild.unban(user)
-        await ctx.send(f"{user.mention} has been unbanned.")
+    async def unban(self, ctx, member):
+        banned_users = await ctx.guild.bans()
+        member_name, member_discriminator = member.split('#')
+
+        for ban in banned_users:
+            user = ban.user
+            if (member_name, member_discriminator) == (user.name, user.discriminator):
+            await ctx.guild.unban(user)
+            await ctx.send(f"{user.mention} has been unbanned.")
 
 def setup(oscar):
     oscar.add_cog(Moderation(oscar))
