16c9d16edb5615ac13285a5c271a33bc6c58dab0
bonelesspizz <38578849+bonelesspizz@users.noreply.github.com>
2020-10-20 21:10:47 +0100

    Fix ban command

--- oscarbot/cogs/moderation.py
+++ oscarbot/cogs/moderation.py
@@ -8,7 +8,7 @@ class Moderation(commands.Cog):
     @commands.command()
     async def ban(self, ctx, member:discord.Member=None, reason=None):
         await member.ban(reason=reason)
-        await ctx.send(f"{user.mention} has been banned.")
+        await ctx.send(f"{member.mention} has been banned.")
 
 def setup(oscar):
     oscar.add_cog(Moderation(oscar))
