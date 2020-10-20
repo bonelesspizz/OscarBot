ea0ed651479f8305724dd589b88b66bc5e7e6676
bonelesspizz <38578849+bonelesspizz@users.noreply.github.com>
2020-10-20 21:00:05 +0100

    Add ctx parameter to ban command

--- oscarbot/cogs/moderation.py
+++ oscarbot/cogs/moderation.py
@@ -6,7 +6,7 @@ class Moderation(commands.Cog):
         self.oscar = oscar
 
     @commands.command()
-    async def ban(self, user, *, reason=None):
+    async def ban(self, ctx, user, *, reason=None):
         await ctx.guild.ban(user)
         await ctx.send(f"{user.mention} has been banned")
 
