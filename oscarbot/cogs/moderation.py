069a023810b378842f8a4e02482aacfa9ace7599
bonelesspizz <38578849+bonelesspizz@users.noreply.github.com>
2020-10-20 21:44:42 +0100

    Await ctx.guild.bans()

--- oscarbot/cogs/moderation.py
+++ oscarbot/cogs/moderation.py
@@ -1,5 +1,6 @@
 import discord
 from discord.ext import commands
+from discord.utils import get
 
 class Moderation(commands.Cog):
     def __init__(self, oscar):
@@ -21,6 +22,12 @@ class Moderation(commands.Cog):
             if (member_name, member_discriminator) == (user.name, user.discriminator):
                 await ctx.guild.unban(user)
                 await ctx.send(f"{user.mention} has been unbanned.")
+
+    @commands.command()
+    async def mute(self, ctx, member:discord.Member=None, reason=None):
+        muted_role = get(member.guild.roles, name=MUTED)
+        await member.add_roles(muted_role)
+        await ctx.send(f"{member.mention} has been muted.")
 
 def setup(oscar):
     oscar.add_cog(Moderation(oscar))
